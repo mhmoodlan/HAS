cfb1953173eeca900366a2f45791318e6aeb5d13
ahmedalahmed <eng.ahmed2013alahmed@Gmail.com>
2016-09-28 20:46:03 +0300

    RoomAPIs.php done. add, remove , update, POST, GET

--- RoomAPIs.php
+++ RoomAPIs.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Ahmed
+ * User: Ahmed Al-Ahmed
  * Date: 28/09/16
  * Time: 12:10 ص
  */
@@ -75,8 +75,10 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 
 
+else
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
 
     if(isset($_POST['adminID']) && isset($_POST['token'])) {
 
@@ -104,12 +106,101 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         # Add new Room ...
 
         if(isset($_POST['sectionID']) && ! empty($_POST['sectionID']) &&
-            Authentication::isSafeID($_POST['sectionID'])){
+            Authentication::isSafeID($_POST['sectionID']) && isset($_POST['cmd'])
+        && $_POST['cmd'] == 'add'){
+
+
+            try{
+
+                $stmt = $conn->prepare("INSERT INTO Room(sectionID) VALUES (:ID)");
+
+                $stmt->bindValue(":ID", $_POST['sectionID']);
+
+                $stmt->execute();
+
+                $status = 1;
+                $msg = "New Room Added.";
+
+                echo json_encode(array("status" => $status, "message" => $msg));
+                exit(0);
+
+            }catch (PDOException $e){
+                $status = 5;
+                echo json_encode(array("status" => $status, "message" => $e->getMessage()));
+                exit(0);
+            }
+
 
 
 
 
             }
+
+
+
+        if(isset($_POST['sectionID']) && ! empty($_POST['sectionID']) &&
+            Authentication::isSafeID($_POST['sectionID']) && isset($_POST['cmd'])
+            && $_POST['cmd'] == 'delete' && isset($_POST['roomID']) && !empty($_POST['roomID']
+                && Authentication::isSafeID($_POST['roomID']))){
+
+
+            try{
+
+                $stmt = $conn->prepare("DELETE FROM Room WHERE sectionID=:ID1 AND roomID=:ID2");
+                $stmt->bindValue(":ID1", $_POST['sectionID']);
+                $stmt->bindValue(":ID2", $_POST['roomID']);
+
+                $stmt->execute();
+                $status = 1;
+                $msg = "Room removed successfully.";
+
+                echo json_encode(array("status" => $status, "message" => $msg));
+                exit(0);
+            }catch (PDOException $e){
+                $status = 5;
+                echo json_encode(array("status" => $status, "message" => $e->getMessage()));
+                exit(0);
+            }
+
+
+
+
+        }
+
+
+
+
+        if(isset($_POST['sectionID']) && ! empty($_POST['sectionID']) &&
+            Authentication::isSafeID($_POST['sectionID']) && isset($_POST['cmd'])
+            && $_POST['cmd'] == 'update' && isset($_POST['roomID']) && !empty($_POST['roomID']
+                && Authentication::isSafeID($_POST['roomID'])) && isset($_POST['newSectionID']) &&
+        !empty($_POST['newSectionID']) && Authentication::isSafeID($_POST['newSectionID'])){
+
+
+            try{
+
+                $stmt = $conn->prepare("UPDATE Room SET sectionID=:NID WHERE sectionID=:ID AND roomID=:RID");
+                $stmt->bindValue(":NID", $_POST['newSectionID']);
+                $stmt->bindValue(":ID", $_POST['sectionID']);
+                $stmt->bindValue(":RID", $_POST['roomID']);
+
+
+                $stmt->execute();
+                $status = 1;
+                $msg = "Room Moved successfully.";
+
+                echo json_encode(array("status" => $status, "message" => $msg));
+                exit(0);
+            }catch (PDOException $e){
+                $status = 5;
+                echo json_encode(array("status" => $status, "message" => $e->getMessage()));
+                exit(0);
+            }
+
+
+
+
+        }
 
 
 
