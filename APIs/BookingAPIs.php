137462cce0ebd0abad5066eae4bcdbc43c2340e5
Mahmoud Aslan <mahmoudaslan@outlook.com>
2016-09-30 13:33:46 +0300

    Add cancel booking functionality

--- APIs/BookingAPIs.php
+++ APIs/BookingAPIs.php
@@ -7,11 +7,12 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(Authentication::isLogedIn()){
+	if(Authentication::isLoggedIn()){
 		if(isset($_POST['userID']) && !empty($_POST['userID']) && Authentication::isSafeID($_POST['userID']) &&
 		   isset($_POST['roomID']) && !empty($_POST['roomID']) && Authentication::isSafeID($_POST['roomID']) &&
 		   isset($_POST['fromDate']) && !empty($_POST['fromDate']) &&
-		   isset($_POST['toDate']) && !empty($_POST['toDate'])) {
+		   isset($_POST['toDate']) && !empty($_POST['toDate']) &&
+		   isset($_POST['cmd']) && !empty($_POST['cmd']) && $_POST['cmd'] == 'add_booking') {
 			
 			try {
 				$fromDate = date('Y-m-d', strtotime($_POST['fromDate']));
@@ -34,12 +35,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$status = 1;
 			echo json_encode(array('status'=>$status, 'data'=>$msg));
 			exit();
-		} else {
-			$msg = 'Some info missing!';
-			$status = 3;
-			echo json_encode(array('status'=>$status, 'data'=>$msg));
-			exit();
-		}
+		} else // handling cancel booking 
+			if(Authentication::checkInput($_POST['userID']) &&
+			   Authentication::checkInput($_POST['roomID']) &&
+			   Authentication::checkInput($_POST['cmd']) && $_POST['cmd'] == 'delete_booking') {
+				try {
+					$stmt = $conn->prepare('DELETE FROM roombooking WHERE roomID = :rid AND userID = :uid');
+					$stmt->bindValue(':rid', $_POST['roomID']);
+					$stmt->bindValue(':uid', $_POST['userID']);
+					$stmt->execute();
+				} catch(PDOException $e ) {
+					$msg = $e->getMessage();
+					$status = 3;
+					echo json_encode(array('status'=>$status, 'data'=>$msg));
+					exit();
+				}
+				$msg = 'Booking deleted successfuly!';
+				$status = 1;
+				echo json_encode(array('status'=>$status, 'data'=>$msg));
+				exit();
+			} else {
+				$msg = 'Some info missing!';
+				$status = 3;
+				echo json_encode(array('status'=>$status, 'data'=>$msg));
+				exit();
+			}			
+		
+	
+		
 	} else {
 		$msg = 'User not logged in!';
 		$status = 3;
